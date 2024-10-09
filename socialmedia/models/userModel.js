cd96bdaa198fc2bad37cbaa4c4ad10e1e85026f0
davisouzak <juliejonatasaraujo@gmail.com>
2024-10-09 19:27:36 -0300

    fix: lint

--- socialmedia/models/userModel.js
+++ socialmedia/models/userModel.js
@@ -1,4 +1,4 @@
-const db = require('./db');
+const db = require('./db')
 
 const createTable = () => {
   const criarUsuario = `
@@ -6,30 +6,30 @@ const createTable = () => {
       id INT AUTO_INCREMENT PRIMARY KEY,
       nome VARCHAR(100) NOT NULL,
       senha VARCHAR(100) NOT NULL
-    );
+    )
   `
   db.query(criarUsuario, (err, result) => {
     if (err) {
-      console.error('Erro ao criar tabela:', err);
+      console.error('Erro ao criar tabela:', err)
       return
     }
-    console.log('Tabela "usuarios" criada com sucesso!');
+    console.log('Tabela "usuarios" criada com sucesso!')
   })
 }
 
 const getUsers = (callback) => {
-  const query = 'SELECT id, nome FROM usuarios';
-  db.query(query, callback);
+  const query = 'SELECT id, nome FROM usuarios'
+  db.query(query, callback)
 }
 
 const insertUser = (user, callback) => {
-  const query = 'INSERT INTO usuarios (nome, senha) VALUES (?, ?)';
-  db.query(query, [user.nome, user.senha], callback);
+  const query = 'INSERT INTO usuarios (nome, senha) VALUES (?, ?)'
+  db.query(query, [user.nome, user.senha], callback)
 }
 
 const findUserByName = (nome, callback) => {
-  const query = `SELECT * FROM usuarios WHERE nome='${nome}'`;
-  db.query(query, callback);
+  const query = `SELECT * FROM usuarios WHERE nome='${nome}'`
+  db.query(query, callback)
 }
 
-module.exports = { createTable, getUsers, insertUser, findUserByName };
+module.exports = { createTable, getUsers, insertUser, findUserByName }
